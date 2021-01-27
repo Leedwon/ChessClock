586697e27763af5e96ed7817c2b93696f1a949b6
Jakub Ledwoń <leedwon12@gmail.com>
2021-01-27 23:24:03 +0100

    workaround for white button ripple

--- app/src/main/java/com/ledwon/jakub/chessclock/feature/clock/widget/ClockButton.kt
+++ app/src/main/java/com/ledwon/jakub/chessclock/feature/clock/widget/ClockButton.kt
@@ -1,14 +1,13 @@
 package com.ledwon.jakub.chessclock.feature.clock.widget
 
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.TextUnit
 import com.ledwon.jakub.chessclock.feature.clock.PlayerDisplay
+import com.ledwon.jakub.chessclock.ui.yellowRipple
 
 @Composable
 fun ClockButton(
@@ -25,6 +24,15 @@ fun ClockButton(
             backgroundColor = when (player) {
                 is PlayerDisplay.White -> Color.White
                 is PlayerDisplay.Black -> Color.Black
+            },
+            contentColor = when (player) {
+                //hack for ripple not working properly in dark mode - it is compose bug probably?
+                is PlayerDisplay.White -> if (MaterialTheme.colors.isLight) {
+                    Color.Black
+                } else {
+                    yellowRipple
+                }
+                is PlayerDisplay.Black -> Color.White
             },
             disabledBackgroundColor = when (player) {
                 is PlayerDisplay.White -> Color.White
