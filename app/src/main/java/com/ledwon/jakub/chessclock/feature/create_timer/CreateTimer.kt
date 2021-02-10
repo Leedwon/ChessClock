334048a7361f4be11345c4ae12a9709fbc8d144e
leedwon <goferek123>
2021-02-10 19:43:11 +0100

    working NumberPicker.kt implementation

--- app/src/main/java/com/ledwon/jakub/chessclock/feature/create_timer/CreateTimer.kt
+++ app/src/main/java/com/ledwon/jakub/chessclock/feature/create_timer/CreateTimer.kt
@@ -1,11 +1,23 @@
 package com.ledwon.jakub.chessclock.feature.create_timer
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import timber.log.Timber
 
 @Composable
 fun CreateTimerScreen() {
-    Text(text = "todo")
+    Box(modifier = Modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+        NumberPicker(
+            0..50,
+            modifier = Modifier.background(Color.Green),
+            onValueChangeListener = { Timber.d("dupa $it") }
+        )
+    }
 }
+
