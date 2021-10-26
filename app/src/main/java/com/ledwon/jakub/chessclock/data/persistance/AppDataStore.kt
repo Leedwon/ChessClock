47378e1b0eb006a82a9f803fea9f89fe55949514
leedwon <leedwon12@gmail.com>
2021-10-26 17:00:32 +0200

    add import for Preferences

--- app/src/main/java/com/ledwon/jakub/chessclock/data/persistance/AppDataStore.kt
+++ app/src/main/java/com/ledwon/jakub/chessclock/data/persistance/AppDataStore.kt
@@ -3,11 +3,12 @@ package com.ledwon.jakub.chessclock.data.persistance
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.SharedPreferencesMigration
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 
 object AppDataStore {
 
-    val Context.dataStore: DataStore<androidx.datastore.preferences.core.Preferences> by preferencesDataStore(
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
         name = "settings",
         produceMigrations = { context ->
             listOf(
