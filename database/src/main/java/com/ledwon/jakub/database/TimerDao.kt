601d707d8c48569655e85fffb99d1056426d0647
leedwon <leedwon12@gmail.com>
2021-11-07 18:55:34 +0100

    add all TimerDatabase related classed to database module

---
+++ database/src/main/java/com/ledwon/jakub/database/TimerDao.kt
@@ -0,0 +1,29 @@
+package com.ledwon.jakub.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.ledwon.jakub.database.model.Timer
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TimerDao {
+    @Query("SELECT * from timer ORDER BY isFavourite desc, id desc")
+    fun getAllTimers(): Flow<List<Timer>>
+
+    @Query("SELECT * from timer where id = :timerId")
+    suspend fun getById(timerId: Int): Timer
+
+    @Insert
+    suspend fun insertTimers(timers: List<Timer>)
+
+    @Delete
+    suspend fun deleteTimer(timer: Timer)
+
+    @Delete
+    suspend fun deleteTimers(timers: List<Timer>)
+
+    @Query("UPDATE timer set isFavourite=:isFavourite where id=:id")
+    suspend fun updateFavouriteStatus(id: Int, isFavourite: Boolean)
+}
