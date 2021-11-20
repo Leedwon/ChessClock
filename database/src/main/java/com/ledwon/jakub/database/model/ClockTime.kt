0c23559980897f41f45824d546a28a8f239c1bfe
leedwon <leedwon12@gmail.com>
2021-11-20 18:39:42 +0100

    remove unneeded stuff from ClockTime model

--- database/src/main/java/com/ledwon/jakub/database/model/ClockTime.kt
+++ database/src/main/java/com/ledwon/jakub/database/model/ClockTime.kt
@@ -5,27 +5,4 @@ internal data class ClockTime(
     val minutes: Int = 0,
     val hours: Int = 0,
     val increment: Int = 0
-) {
-    private fun formatToTwoDigitsString(value: Int) = if (value < 10) "0$value" else value
-
-    val secondsSum: Int
-        get() = seconds + minutes * 60 + hours * 60 * 60
-
-    override fun toString(): String {
-        return if (hours == 0) {
-            textWithoutHours
-        } else {
-            textWithHours
-        }
-    }
-
-    private val textWithHours: String
-        get() = "${formatToTwoDigitsString(hours)}:${formatToTwoDigitsString(minutes)}:${
-            formatToTwoDigitsString(
-                seconds
-            )
-        }"
-
-    private val textWithoutHours: String
-        get() = textWithHours.substring(3)
-}
+)
