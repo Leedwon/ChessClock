d68da379eb8b8bf78523c9928755f90fea63732b
leedwon <leedwon12@gmail.com>
2022-02-13 19:12:27 +0100

    use position randomizer in ClockViewModel

--- app/src/main/java/com/ledwon/jakub/chessclock/di/clock/ClockModule.kt
+++ app/src/main/java/com/ledwon/jakub/chessclock/di/clock/ClockModule.kt
@@ -1,6 +1,7 @@
 package com.ledwon.jakub.chessclock.di.clock
 
 import com.ledwon.jakub.chessclock.feature.clock.ClockViewModel
+import com.ledwon.jakub.chessclock.feature.clock.PositionRandomizer
 import com.ledwon.jakub.chessclock.feature.clock.util.PauseClock
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -8,12 +9,15 @@ import org.koin.dsl.module
 val clockModule = module {
     factory { PauseClock() }
 
+    factory { PositionRandomizer() }
+
     viewModel { parameters ->
         ClockViewModel(
             clockInitialData = parameters[0],
             settingsRepository = get(),
             pauseClock = get(),
-            analyticsManager = get()
+            analyticsManager = get(),
+            positionRandomizer = get()
         )
     }
 }
