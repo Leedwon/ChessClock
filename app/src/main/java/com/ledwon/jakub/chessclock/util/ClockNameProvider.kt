aa785109a69272965de3a0e4ae08f3cfc4861aef
leedwon <leedwon12@gmail.com>
2022-02-20 22:39:08 +0100

    update DeferrableString so it can work with normal strings as well

--- app/src/main/java/com/ledwon/jakub/chessclock/util/ClockNameProvider.kt
+++ app/src/main/java/com/ledwon/jakub/chessclock/util/ClockNameProvider.kt
@@ -6,42 +6,42 @@ import com.ledwon.jakub.chessclock.model.Clock
 
 object ClockNameProvider {
 
-    fun Clock.obtainDeferrableName(): DeferrableString = createClockName(
+    fun Clock.obtainDeferrableName(): ResDeferrableString = createClockName(
         whitePlayerTime = this.whitePlayerTime,
         blackPlayerTime = this.blackPlayerTime
     )
 
-    private fun createClockName(whitePlayerTime: PlayerTime, blackPlayerTime: PlayerTime): DeferrableString {
+    private fun createClockName(whitePlayerTime: PlayerTime, blackPlayerTime: PlayerTime): ResDeferrableString {
         return when {
-            whitePlayerTime != blackPlayerTime -> DeferrableString(R.string.clock_name_custom)
+            whitePlayerTime != blackPlayerTime -> ResDeferrableString(R.string.clock_name_custom)
             else -> createNameForClock(whitePlayerTime)
         }
     }
 
-    private fun createNameForClock(PlayerTime: PlayerTime): DeferrableString {
+    private fun createNameForClock(PlayerTime: PlayerTime): ResDeferrableString {
         val incrementSuffix = createSuffixIncrement(PlayerTime.increment)
         val twoDigitMinutes =
             ":${formatToTwoDigitsString(PlayerTime.minutes)}".takeIf { PlayerTime.minutes > 0 } ?: ""
         val twoDigitSeconds =
             ":${formatToTwoDigitsString(PlayerTime.seconds)}".takeIf { PlayerTime.seconds > 0 } ?: ""
         return when {
-            PlayerTime.hours > 0 -> DeferrableString(
+            PlayerTime.hours > 0 -> ResDeferrableString(
                 resId = R.string.clock_name_classic,
                 formatArgs = listOf("${PlayerTime.hours}$twoDigitMinutes$twoDigitSeconds$incrementSuffix")
             )
-            PlayerTime.minutes > 10 -> DeferrableString(
+            PlayerTime.minutes > 10 -> ResDeferrableString(
                 resId = R.string.clock_name_rapid,
                 formatArgs = listOf("${PlayerTime.minutes}$twoDigitSeconds$incrementSuffix")
             )
-            PlayerTime.minutes in 3..10 -> DeferrableString(
+            PlayerTime.minutes in 3..10 -> ResDeferrableString(
                 resId = R.string.clock_name_blitz,
                 formatArgs = listOf("${PlayerTime.minutes}$twoDigitSeconds$incrementSuffix")
             )
-            PlayerTime.minutes > 0 -> DeferrableString(
+            PlayerTime.minutes > 0 -> ResDeferrableString(
                 resId = R.string.clock_name_bullet,
                 formatArgs = listOf("${PlayerTime.minutes}$twoDigitSeconds$incrementSuffix")
             )
-            else -> DeferrableString(
+            else -> ResDeferrableString(
                 resId = R.string.clock_name_bullet,
                 formatArgs = listOf("${PlayerTime.seconds}s$incrementSuffix")
             )
